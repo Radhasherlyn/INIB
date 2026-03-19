2b7ae95349e604acd54b3b3eae4b5a30ca69fad7
Radhasherlyn <radhasherlyngujjula@gmail.com>
2026-03-19 22:11:59 +0530

    portfolio

--- script.js
+++ script.js
@@ -1,24 +1,5 @@
-let expression = "";
-
-let expressionDisplay = document.getElementById("expression");
-let resultDisplay = document.getElementById("result");
-
-function appendValue(value){
-expression += value;
-expressionDisplay.textContent = expression;
-}
-
-function clearDisplay(){
-expression="";
-expressionDisplay.textContent="";
-resultDisplay.textContent="";
-}
-
-function calculate(){
-try{
-let result = eval(expression);
-resultDisplay.textContent = result;
-}catch{
-resultDisplay.textContent="Error";
-}
+function scrollToSection() {
+    document.getElementById("projects").scrollIntoView({
+        behavior: "smooth"
+    });
 }
